e7dbaab25f4b8d4de191e80989ae3d2424765575
vinoduiconnect <vinod.p@uiconnect.in>
2021-09-29 13:27:56 +0530

    title

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
 
-    <Navbar :logo="$site.themeConfig.logo" />
+    <Navbar :logo="$site.themeConfig.logo" :logotitle="$site.themeConfig.logotitle" />
 
     <div class="container">
 
